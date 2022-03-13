c7d1c539e41dc8644333da1127cd2bec60f80934
Sammy <samalaniz25@gmail.com>
2022-03-13 10:11:29 -0600

    got nosetests to work

---
+++ tests/controlled_test.py
@@ -0,0 +1,21 @@
+import unittest
+import os.path
+import compute_highest_affinity
+
+class ControlledTests(unittest.TestCase):
+
+    def test_one(self):
+        site_list = ["a.com", "b.com", "a.com", "b.com", "a.com", "c.com"]
+        user_list = ["andy", "andy", "bob", "bob", "charlie", "charlie"]
+        time_list = [
+            1238972321, 1238972456, 1238972618, 1238972899, 1248472489, 1258861829
+        ]
+        
+        computed_result = compute_highest_affinity.highest_affinity(
+            site_list, user_list, time_list)
+        expected_result = ("a.com", "b.com")
+
+        self.assertEqual(computed_result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
